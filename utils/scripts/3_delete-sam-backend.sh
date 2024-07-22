55dc223bb0d2184f7ee9b9aed8c11af977d2e0a2
Andres Solorzano <andres.solorzano.-nd@disney.com>
2024-07-22 18:32:12 -0500

    Updating script files

--- utils/scripts/3_delete-sam-backend.sh
+++ utils/scripts/3_delete-sam-backend.sh
@@ -15,10 +15,9 @@ echo "- Workloads Environment: $AWS_WORKLOADS_ENV"
 
 echo ""
 echo "DELETING SAM PROJECT FROM <WORKLOADS> ACCOUNT..."
-sam delete --stack-name "city-data-function"    \
-    --config-env "$AWS_WORKLOADS_ENV"           \
-    --no-prompts                                \
-    --profile "$AWS_WORKLOADS_PROFILE"
+sam delete        \
+  --no-prompts    \
+  --profile "$AWS_WORKLOADS_PROFILE"
 
 ### REMOVE SAM CONFIGURATION FILES
 rm -rf "$WORKING_DIR"/functions/.aws-sam
